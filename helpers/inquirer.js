d5cf25b29f68a655a938cb86ac33db697afc791f
Iveth Plata <karen.platahdz22@outlook.es>
2023-04-02 00:05:47 -0600

    Menu

--- helpers/inquirer.js
+++ helpers/inquirer.js
@@ -8,31 +8,15 @@ const preguntas = [
         message: '¿Qué desea hacer?',
         choices: [
             {
-                value: '1',
-                name: `${'1.'.green} Crear Tarea`
+                value: 1,
+                name: `${'1.'.green} Buscar Ciudad`
             },
             {
-                value: '2',
-                name: `${'2.'.green} Listar Tareas`
+                value: 2,
+                name: `${'2.'.green} Historial`
             },
             {
-                value: '3',
-                name: `${'3.'.green} Listar Tareas Completadas`
-            },
-            {
-                value: '4',
-                name: `${'4.'.green} Listar Tareas Pendientes`
-            },
-            {
-                value: '5',
-                name: `${'5.'.green} Completar Tareas(s)`
-            },
-            {
-                value: '6',
-                name: `${'6.'.green} Borrar Tareas`
-            },
-            {
-                value: '0',
+                value: 0,
                 name: `${'0.'.green} Salir`
             }
         ]
--- index.js
+++ index.js
@@ -1,8 +1,30 @@
-const { leerInput } = require('./helpers/inquirer');
+const { pausa } = require('./helpers/inquirer');
+const { leerInput, inquirerMenu, confirmar } = require('./helpers/inquirer');
 
 main = async() => {
-    const texto = await leerInput('Hola: ');
-    console.log(texto);
+    let opt = -1;
+
+    do {
+        opt = await inquirerMenu();
+        switch (opt) {
+            case 0:
+                // Salir
+                const ok = await confirmar('¿Estas seguro?');
+                if(ok)
+                    console.log('\nAdios :D'.yellow);
+                else
+                    opt = -1;
+            break;
+            case 1:
+                // Buscar Ciudad
+            break;
+            case 2:
+                // Historial
+            break;
+        }
+        await pausa();
+        console.clear();
+    } while (opt != 0);
 }
 
 main();
