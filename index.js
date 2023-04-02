08b63d699d6e026e7f179a48d9418aa5d083d8b4
Iveth Plata <karen.platahdz22@outlook.es>
2023-04-02 00:12:18 -0600

    Modelo

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const { pausa } = require('./helpers/inquirer');
 const { leerInput, inquirerMenu, confirmar } = require('./helpers/inquirer');
+const Busquedas = require('./models/busquedas');
 
 main = async() => {
     let opt = -1;
+    const busquedas = new Busquedas();
 
     do {
         opt = await inquirerMenu();
@@ -17,6 +19,19 @@ main = async() => {
             break;
             case 1:
                 // Buscar Ciudad
+                const lugar = await leerInput('Ciudad: ');
+                console.log(lugar);
+                // Buscar el lugar
+                // Seleccionar el lugar
+                // Datos del clima
+                // Mostrar resultados
+                console.log('\nInformación de la ciudad\n'.green);
+                console.log('Ciudad: ', );
+                console.log('Lat: ', );
+                console.log('Lng: ', );
+                console.log('Temperatura: ', );
+                console.log('Minima: ', );
+                console.log('Maxima: ', );
             break;
             case 2:
                 // Historial
---
+++ models/busquedas.js
@@ -0,0 +1,17 @@
+class Busquedas {
+    historial = [];
+
+    constructor() {
+        // TODO: Leer db si existe
+    }
+
+    async ciudad(lugar='') {
+        // TODO: Peticion http
+        console.log(lugar);
+
+        // TODO: Retornar los lugares que coincidan con lugar
+        return [];
+    }
+}
+
+module.exports = Busquedas;
